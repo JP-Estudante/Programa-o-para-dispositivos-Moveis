2350c8f6cb452068d97715e5fa72ca5c9aea098d
Your Name <you@example.com>
2024-10-25 20:43:45 -0300

    Adicionando usuario login

--- lib/screen/tela_login.dart
+++ lib/screen/tela_login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:my_app/models/usuario_login.dart';
+import 'package:my_app/screen/tela_principal.dart';
+import 'package:my_app/services/servico_autenticacao.dart';
 
 class TelaLogin extends StatefulWidget {
   const TelaLogin({super.key});
@@ -9,56 +11,96 @@ class TelaLogin extends StatefulWidget {
 }
 
 class _TelaLoginState extends State<TelaLogin> {
+  final formKey = GlobalKey<FormState>();
+  final usuarioController = TextEditingController(text: 'emilys');
+  final senhaController = TextEditingController(text: 'emilyspass');
+  bool fazendoLogin = false;
+
+  final servicoAutenticacao = ServicoAutenticacao();
+
+  Future<void> registraLogin() async {
+    setState(() {
+      fazendoLogin = true;
+    });
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text('Processando Login')));
+
+    try {
+      final UsuarioLogin = await servicoAutenticacao.login(
+          usuarioController.text, senhaController.text);
+      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+      if (UsuarioLogin != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => TelaPrincipal()));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.toString()),
+        backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      ));
+    } finally {
+      setState(() {
+        fazendoLogin = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: Form(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Opacity(
-              opacity: 0.4, 
-              child: Icon(Icons.person_3_rounded, size: 100, color: Colors.indigoAccent),
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Opacity(
+                opacity: 0.4,
+                child: Icon(Icons.person_3_rounded,
+                    size: 100, color: Colors.indigoAccent),
               ),
               Padding(
                 padding: const EdgeInsets.all(35),
                 child: TextFormField(
+                  controller: usuarioController,
                   decoration: InputDecoration(
-                    labelText: 'ID do SIDINEY',
-                    prefixIcon: Icon(Icons.pan_tool_sharp),
-                    suffixIcon: IconButton(
-                      icon: Icon(Icons.visibility),
-                      onPressed: (){},
-                    ),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))
-                  ),
+                      labelText: 'Usuario',
+                      prefixIcon: Icon(Icons.pan_tool_sharp),
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.visibility),
+                        onPressed: () {},
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: TextFormField(
+                  controller: senhaController,
                   obscureText: true,
                   decoration: InputDecoration(
                       labelText: "Senha",
                       prefixIcon: Icon(Icons.password),
                       suffixIcon: IconButton(
                         icon: Icon(Icons.visibility),
-                        onPressed: (){},
+                        onPressed: () {},
                       ),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(20))
-                  ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20))),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: ElevatedButton(
-                  child: Text("Entrar"),
-                  onPressed: (){},
-                  ),
-                )
-          ],
-          )
-      ),
+                  child: fazendoLogin
+                      ? CircularProgressIndicator()
+                      : Text("Entrar"),
+                  onPressed: fazendoLogin ? null : registraLogin,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
